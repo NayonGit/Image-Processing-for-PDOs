664354bdfea2a2cde1356b7ec58c3eb4fd7b7b6a
mathildebodet <mathildebodet@orange.fr>
2026-02-04 11:08:17 +0100

    dinov2 detr

--- configs/__init__.py
+++ configs/__init__.py
@@ -9,4 +9,4 @@ with open(os.path.join(os.path.dirname(__file__), 'dataset_config.yaml'), 'r') a
 with open(os.path.join(os.path.dirname(__file__), 'train_config.yaml'), 'r') as file:
     train_config = edict(yaml.safe_load(file))
 
-default_data_dir = os.path.join(Path(os.path.dirname(__file__)).parent.parent, 'data')
+default_data_dir = os.path.join(Path(os.path.dirname(__file__)).parent.parent, 'Image-Processing-for-PDOs/data')
